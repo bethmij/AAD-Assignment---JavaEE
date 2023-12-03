af74be4bf24715d53c7e236c6115e92627ee91cd
Dinarbrooke <bethmij@gmail.com>
2023-12-03 10:44:59 +0530

    add filter

--- back-end/src/main/java/lk/ijse/gdse66/backend/filter/MyFilter.java
+++ back-end/src/main/java/lk/ijse/gdse66/backend/filter/MyFilter.java
@@ -13,7 +13,7 @@ public class MyFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 
     @Override
