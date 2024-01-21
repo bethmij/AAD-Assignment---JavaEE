1296a3ad815aba9807b7269ce88792ba8e65c0cd
Dinarbrooke <bethmij@gmail.com>
2024-01-21 11:08:00 +0530

    updated Customer do post

--- back-end/src/main/java/lk/ijse/gdse66/backend/api/CustomerServlet.java
+++ back-end/src/main/java/lk/ijse/gdse66/backend/api/CustomerServlet.java
@@ -3,7 +3,6 @@ package lk.ijse.gdse66.backend.api;
 import jakarta.json.*;
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
-import jakarta.servlet.ServletContext;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -35,76 +34,17 @@ public class CustomerServlet extends HttpServlet {
 
         BasicDataSource source = (BasicDataSource) req.getServletContext().getAttribute("bds");
 
-
-
         switch (option) {
             case "GET": {
-                List<CustomerEntity> customerList = new ArrayList<>();
-                CustomerEntity customer;
-
-                    String sql = "SELECT * FROM customer";
-                    ResultSet resultSet = CrudUtil.execute(sql, source,resp);
-
-                    try {
-                        while (resultSet.next()) {
-                        String id = resultSet.getString(1);
-                        String name = resultSet.getString(2);
-                        String address = resultSet.getString(3);
-                        double salary = resultSet.getDouble(4);
-
-                        customer = new CustomerEntity(id, name, address, salary);
-                        customerList.add(customer);
-                    }
-                        Jsonb jsonb = JsonbBuilder.create();
-                        jsonb.toJson(customerList, resp.getWriter());
-
-                    } catch (SQLException e) {
-                        resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-                        resp.getWriter().println("Server Error! Please Try Again");
-                    }
-                    break;
+                getAllCustomer(resp, source);
+                break;
             }
             case "SEARCH": {
-                String cusID = req.getParameter("cusID");
-
-                List<CustomerEntity> customerList = new ArrayList<>();
-                CustomerEntity customer;
-
-                String sql = "SELECT * FROM customer";
-                ResultSet resultSet = CrudUtil.execute(sql, source,resp);
-
-                try {
-                    while (resultSet.next()) {
-                        String id = resultSet.getString(1);
-                        String name = resultSet.getString(2);
-                        String address = resultSet.getString(3);
-                        double salary = resultSet.getDouble(4);
-
-                        customer = new CustomerEntity(id, name, address, salary);
-                        customerList.add(customer);
-                    }
-                    Jsonb jsonb = JsonbBuilder.create();
-                    jsonb.toJson(customerList, resp.getWriter());
-
-                } catch (SQLException e) {
-                    resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-                    resp.getWriter().println("Server Error! Please Try Again");
-                }
+                getCustomerById(req, resp, source);
                 break;
             }
             case "ID":
-                try (Connection connection = source.getConnection()) {
-                    PreparedStatement preparedStatement = connection.prepareStatement("SELECT id FROM customer");
-                    ResultSet resultSet = preparedStatement.executeQuery();
-                    JsonArrayBuilder cusIDList = Json.createArrayBuilder();
-
-                    while (resultSet.next()) {
-                        cusIDList.add(resultSet.getString(1));
-                    }
-                    sendMsg(resp, cusIDList.build(), "Got the list", 200);
-                } catch (SQLException e) {
-                    sendMsg(resp, JsonValue.EMPTY_JSON_ARRAY, e.getLocalizedMessage(), 400);
-                }
+                getCusIDList(resp, source);
                 break;
         }
     }
@@ -112,29 +52,25 @@ public class CustomerServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("application/json");
-        String id = req.getParameter("cusID");
-        String name = req.getParameter("cusName");
-        String address = req.getParameter("cusAddress");
-        String salary = req.getParameter("cusSalary");
+        BasicDataSource source = (BasicDataSource) req.getServletContext().getAttribute("bds");
 
-
+        CustomerEntity customer = JsonbBuilder.create().fromJson(req.getReader(),CustomerEntity.class);
+        String id = customer.getId();
+        String name = customer.getName();
+        String address = customer.getAddress();
+        double salary = customer.getSalary();
 
         try (Connection connection = source.getConnection()) {
-            PreparedStatement pst = connection.prepareStatement(
-                    "INSERT INTO company.customer (id, name, address, salary) VALUES (?,?,?,?)");
-            pst.setString(1,id);
-            pst.setString(2,name);
-            pst.setString(3,address);
-            pst.setString(4,salary);
-            boolean isAdded = pst.executeUpdate() > 0;
+            String sql = "INSERT INTO company.customer (id, name, address, salary) VALUES (?,?,?,?)";
+            boolean isSaved = CrudUtil.execute(sql, connection, id, name, address, salary);
 
-            if(isAdded){
-                sendMsg(resp, JsonValue.EMPTY_JSON_ARRAY, "Customer Added Successfully", 200);
+            if(isSaved){
+                sendServerMsg(resp, HttpServletResponse.SC_OK, "Customer Saved Successfully!");
             }else {
-                sendMsg(resp, JsonValue.EMPTY_JSON_ARRAY, "Customer Addition Failed", 400);
+                sendServerMsg(resp, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Server error! Please try again");
             }
         } catch (SQLException e) {
-            sendMsg(resp, JsonValue.EMPTY_JSON_ARRAY, e.getLocalizedMessage(), 400);
+            sendServerMsg(resp, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Server error! Please try again");
         }
 
     }
@@ -142,20 +78,20 @@ public class CustomerServlet extends HttpServlet {
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("application/json");
-        JsonReader reader = Json.createReader(req.getReader());
-        JsonObject jsonObject = reader.readObject();
-        String cusID = jsonObject.getString("cusID");
-        String cusName = jsonObject.getString("cusName");
-        String cusAddress = jsonObject.getString("cusAddress");
-        String cusSalary = jsonObject.getString("cusSalary");
+        BasicDataSource source = (BasicDataSource) req.getServletContext().getAttribute("bds");
+
+        CustomerEntity customer = JsonbBuilder.create().fromJson(req.getReader(),CustomerEntity.class);
+        String id = customer.getId();
+        String name = customer.getName();
+        String address = customer.getAddress();
+        double salary = customer.getSalary();
 
         try (Connection connection = source.getConnection()){
+            String sql = "UPDATE customer SET name=?, address=?, salary=? WHERE id=?";
+            boolean isUpdared
             PreparedStatement pst = connection.prepareStatement(
-                    "UPDATE customer SET name=?, address=?, salary=? WHERE id=?");
-            pst.setString(1,cusName);
-            pst.setString(2,cusAddress);
-            pst.setString(3,cusSalary);
-            pst.setString(4,cusID);
+                    "");
+
 
             boolean is_updated = pst.executeUpdate() > 0;
             if(is_updated)
@@ -195,5 +131,82 @@ public class CustomerServlet extends HttpServlet {
         builder.add("message",message);
         builder.add("status",status);
         resp.getWriter().println(builder.build());
+    }
+
+
+    private void getAllCustomer(HttpServletResponse resp, BasicDataSource source) throws IOException {
+        List<CustomerEntity> customerList = new ArrayList<>();
+        CustomerEntity customer;
+
+        try (Connection connection = source.getConnection()){
+            String sql = "SELECT * FROM customer";
+            ResultSet resultSet = CrudUtil.execute(sql, connection);
+
+            while (resultSet.next()) {
+                String id = resultSet.getString(1);
+                String name = resultSet.getString(2);
+                String address = resultSet.getString(3);
+                double salary = resultSet.getDouble(4);
+
+                customer = new CustomerEntity(id, name, address, salary);
+                customerList.add(customer);
+            }
+            Jsonb jsonb = JsonbBuilder.create();
+            jsonb.toJson(customerList, resp.getWriter());
+
+        } catch (SQLException e) {
+            sendServerMsg(resp, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Server error! Please try again");
+        }
+    }
+
+
+
+    private void getCustomerById(HttpServletRequest req, HttpServletResponse resp, BasicDataSource source) throws IOException {
+        String cusID = req.getParameter("cusID");
+        CustomerEntity customer = null;
+
+        try (Connection connection = source.getConnection()){
+            String sql = "SELECT * FROM customer WHERE id=?";
+            ResultSet resultSet = CrudUtil.execute(sql, connection, cusID);
+
+            if (resultSet.next()) {
+                String id = resultSet.getString(1);
+                String name = resultSet.getString(2);
+                String address = resultSet.getString(3);
+                double salary = resultSet.getDouble(4);
+
+                customer = new CustomerEntity(id, name, address, salary);
+
+            }
+            Jsonb jsonb = JsonbBuilder.create();
+            jsonb.toJson(customer, resp.getWriter());
+
+        } catch (SQLException e) {
+            sendServerMsg(resp, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Server error! Please try again");
+        }
+    }
+
+    private void getCusIDList(HttpServletResponse resp, BasicDataSource source) throws IOException {
+        List<String> cusIdList = new ArrayList<>();
+
+        try (Connection connection = source.getConnection()){
+            String sql = "SELECT id FROM customer";
+            ResultSet resultSet = CrudUtil.execute(sql, connection);
+
+            while (resultSet.next()) {
+                String id = resultSet.getString(1);
+                cusIdList.add(id);
+            }
+            Jsonb jsonb = JsonbBuilder.create();
+            jsonb.toJson(cusIdList, resp.getWriter());
+
+        } catch (SQLException e) {
+            sendServerMsg(resp, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Server error! Please try again");
+        }
+    }
+
+    private void sendServerMsg(HttpServletResponse resp, int status, String msg) throws IOException {
+        resp.setStatus(status);
+        resp.getWriter().println(msg);
     }
 }
--- back-end/src/main/java/lk/ijse/gdse66/backend/util/CrudUtil.java
+++ back-end/src/main/java/lk/ijse/gdse66/backend/util/CrudUtil.java
@@ -1,35 +1,28 @@
 package lk.ijse.gdse66.backend.util;
 
 
-import jakarta.servlet.http.HttpServletResponse;
 import org.apache.commons.dbcp.BasicDataSource;
 
 
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class CrudUtil {
 
-    public static <T> T execute(String sql, BasicDataSource source,  Object... params){
+    public static<T> T execute(String sql, Connection connection, Object... params) throws SQLException {
 
-        try (Connection connection = source.getConnection()) {
             PreparedStatement pstm = connection.prepareStatement(sql);
 
             for (int i = 0; i < params.length; i++) {
                 pstm.setObject((i + 1), params[i]);
             }
 
-            if (sql.startsWith("SELECT"))
+            if (sql.trim().startsWith("SELECT")) {
                 return (T) pstm.executeQuery();
-            else
-                return (T) (Boolean) (pstm.executeUpdate() > 0);
+            }else {
+                return (T) (Boolean)(pstm.executeUpdate()>0);
+            }
 
-        } catch (SQLException e) {
-            return null;
-        }
     }
-}
 
+
+}
