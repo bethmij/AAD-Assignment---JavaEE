3e2d4e6bcf8c43f2e0f7ded0f5481448fa9df092
Dinarbrooke <bethmij@gmail.com>
2024-02-01 09:56:05 +0530

    implementing hibernate

---
+++ back-end/src/main/java/lk/ijse/gdse66/backend/filter/MyFilter.java
@@ -0,0 +1,31 @@
+package lk.ijse.gdse66.backend.filter;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class MyFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        String origin = req.getHeader("origin");
+        if(origin == null){
+            res.sendError(HttpServletResponse.SC_BAD_REQUEST, "CORS Policy Violation");
+            return;
+        }
+
+        res.addHeader("Access-Control-Allow-Origin", origin);
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.addHeader("Access-Control-Allow-Headers", "Content-Type");
+
+        chain.doFilter(req,res);
+    }
+
+}
